3519015a4bab54d96ac09529351320cdc99f1522
NevinYasser <nevinyasser52004@gmail.com>
2026-05-06 23:00:26 +0300

    routes.py

--- deposit/routes.py
+++ deposit/routes.py
@@ -1,1 +1,8 @@
+from . import handlers as deposit_handlers
 
+# Format: (Method, Path, Handler_Function)
+# To be collected by router.py
+routes = [
+    ('GET',  '/deposit',          deposit_handlers.deposit_page),
+    ('POST', '/deposit/process',  deposit_handlers.deposit_process),
+]
